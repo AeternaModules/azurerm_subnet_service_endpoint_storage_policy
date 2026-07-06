69a473d87eeff0edb760cd5749f228981c75cbc6
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "subnet_service_endpoint_storage_policies" {
   description = "All subnet_service_endpoint_storage_policy resources"
   value       = azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies
 }
+output "subnet_service_endpoint_storage_policies_definition" {
+  description = "List of definition values across all subnet_service_endpoint_storage_policies"
+  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.definition]
+}
+output "subnet_service_endpoint_storage_policies_location" {
+  description = "List of location values across all subnet_service_endpoint_storage_policies"
+  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.location]
+}
+output "subnet_service_endpoint_storage_policies_name" {
+  description = "List of name values across all subnet_service_endpoint_storage_policies"
+  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.name]
+}
+output "subnet_service_endpoint_storage_policies_resource_group_name" {
+  description = "List of resource_group_name values across all subnet_service_endpoint_storage_policies"
+  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.resource_group_name]
+}
+output "subnet_service_endpoint_storage_policies_tags" {
+  description = "List of tags values across all subnet_service_endpoint_storage_policies"
+  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.tags]
+}
 
