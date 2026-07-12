c6c6d05d8b0a9f2c44821ef84d07cc423fdc25a4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "subnet_service_endpoint_storage_policies" {
-  description = "All subnet_service_endpoint_storage_policy resources"
-  value       = azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies
-}
 output "subnet_service_endpoint_storage_policies_definition" {
-  description = "List of definition values across all subnet_service_endpoint_storage_policies"
-  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.definition]
+  description = "Map of definition values across all subnet_service_endpoint_storage_policies, keyed the same as var.subnet_service_endpoint_storage_policies"
+  value       = { for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : k => v.definition }
 }
 output "subnet_service_endpoint_storage_policies_location" {
-  description = "List of location values across all subnet_service_endpoint_storage_policies"
-  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.location]
+  description = "Map of location values across all subnet_service_endpoint_storage_policies, keyed the same as var.subnet_service_endpoint_storage_policies"
+  value       = { for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : k => v.location }
 }
 output "subnet_service_endpoint_storage_policies_name" {
-  description = "List of name values across all subnet_service_endpoint_storage_policies"
-  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.name]
+  description = "Map of name values across all subnet_service_endpoint_storage_policies, keyed the same as var.subnet_service_endpoint_storage_policies"
+  value       = { for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : k => v.name }
 }
 output "subnet_service_endpoint_storage_policies_resource_group_name" {
-  description = "List of resource_group_name values across all subnet_service_endpoint_storage_policies"
-  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.resource_group_name]
+  description = "Map of resource_group_name values across all subnet_service_endpoint_storage_policies, keyed the same as var.subnet_service_endpoint_storage_policies"
+  value       = { for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : k => v.resource_group_name }
 }
 output "subnet_service_endpoint_storage_policies_tags" {
-  description = "List of tags values across all subnet_service_endpoint_storage_policies"
-  value       = [for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : v.tags]
+  description = "Map of tags values across all subnet_service_endpoint_storage_policies, keyed the same as var.subnet_service_endpoint_storage_policies"
+  value       = { for k, v in azurerm_subnet_service_endpoint_storage_policy.subnet_service_endpoint_storage_policies : k => v.tags }
 }
 
